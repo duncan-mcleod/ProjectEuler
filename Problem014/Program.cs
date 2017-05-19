3d85c66bffe74ea54ca247f737524bc5807b58e4
CAMSPRO\dmcleod <dmcleod@camspro.com>
2017-05-19 13:21:43 -0700

    Problem 14 optimized.

--- Problem014/Program.cs
+++ Problem014/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections;
+using System.Diagnostics;
 
 namespace Problem014 {
     class Program {
         static void Main(string[] args) {
-            int num = BruteForceCollatz();
 
-            Console.WriteLine(num);
+            Stopwatch watch = Stopwatch.StartNew();
+            int num = BruteForceCollatz();
+            watch.Stop();
+            Console.WriteLine($"{num} - {watch.Elapsed}");
+
+            watch.Restart();
+            num = OptimizedCollatz();
+            watch.Stop();
+            Console.WriteLine($"{num} - {watch.ElapsedMilliseconds}");
+
             Console.ReadLine();
         }
+
+        
 
         private static int BruteForceCollatz() {
 
@@ -40,6 +52,62 @@ namespace Problem014 {
                 }
                 length++;
             }
+            return length;
+        }
+
+
+
+
+
+        /**************************************   optimized   **************************************/
+        
+        private static int[] _values;
+
+        private static int OptimizedCollatz() {
+
+            _values = new int[1000000];
+
+            int length = 0,
+                maxLength = 0,
+                maxVal = 0;
+
+
+            for (int i = 2; i < 1000000; i++) {
+
+                length = FindSequenceLengthOptimized(i);
+                if (length > maxLength) {
+                    maxLength = length;
+                    maxVal = i;
+                }
+            }
+
+            return maxVal;
+        }
+
+
+
+        private static int FindSequenceLengthOptimized(long num) {
+
+            int length = 0;
+            long tmp = num;
+
+            while (tmp != 1) {
+
+                if (tmp < 1000000 && _values[tmp] > 0) {
+                    length += _values[tmp];
+                    break;
+                }
+
+                if (tmp % 2 == 0) { //even
+                    tmp /= 2;
+                } else { //odd
+                    tmp = 3 * tmp + 1;
+                }
+                length++;
+            }
+
+
+            _values[num] = length;
             return length;
         }
     }
