54bcb56c4f128abc120a6968a430b8cafb73c174
Duncan McLeod <duncan.mcleod77@gmail.com>
2017-05-19 19:47:31 -0700

    prob 16

---
+++ Problem016/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem016 {
+    class Program {
+
+        //Sum of digits in 2^1000
+        static void Main(string[] args) {
+
+            BitArray bits = new BitArray(1000, false);
+            bits[0] = true;
+
+            
+        }
+    }
+}
